76a1ae96841ebe56aad671734df857257d129176
code_hopper <ameykhondekar01@gmail.com>
2024-03-20 06:58:17 +0530

    nav tabs for institute login and register

--- frontend/src/components/InstituteLR.jsx
+++ frontend/src/components/InstituteLR.jsx
@@ -68,17 +68,29 @@ const InstituteLR = () => {
       <div className='container-fluid'>
         <div className="container">
 
-          <form onSubmit={hadelSubmit}>
-            <input onChange={handelChange} type="text" placeholder='name' name='name' value={institute.name} />
-            <input onChange={handelChange} type="email" placeholder='email' name='email' value={institute.email} />
-            <input onChange={handelChange} type="tel" placeholder='contact' name='contact' value={institute.contact} />
-            <textarea onChange={handelChange} name="address" id="" cols="30" rows="10" value={institute.address}></textarea>
-            <input onChange={handelChange} type="text" placeholder='password' name='password' value={institute.password} />
-
-            <button className='btn btn-success' type='submit'>Submit</button>
-
-          </form>
-
+          <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#login-form-container" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Login</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#register-form-container" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Register</button>
+            </li>
+          </ul>
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="login-form-container" role="tabpanel" aria-labelledby="pills-home-tab">
+              {/* login form */}
+            </div>
+            <div class="tab-pane fade" id="register-form-container" role="tabpanel" aria-labelledby="pills-profile-tab">
+              <form onSubmit={hadelSubmit}>
+                <input onChange={handelChange} type="text" placeholder='name' name='name' value={institute.name} />
+                <input onChange={handelChange} type="email" placeholder='email' name='email' value={institute.email} />
+                <input onChange={handelChange} type="tel" placeholder='contact' name='contact' value={institute.contact} />
+                <textarea onChange={handelChange} name="address" id="" cols="30" rows="10" value={institute.address}></textarea>
+                <input onChange={handelChange} type="text" placeholder='password' name='password' value={institute.password} />
+                <button className='btn btn-success' type='submit'>Submit</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
